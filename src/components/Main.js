b102c834edcfc02f29ad623cecba322184e148fc
Rafael <rafael.2018.ads@gmail.com>
2022-11-10 08:29:39 -0300

    SYNC

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 
-//Form
-import { FaPlus } from 'react-icons/fa';
-
-//Tarefas
-import { FaEdit, FaWindowClose } from 'react-icons/fa';
-
+import Form from './Form';
+import Tarefas from './Tarefas';
 import './Main.css';
 
 export default class Main extends Component {
@@ -95,38 +91,17 @@ export default class Main extends Component {
       <div className='main'>
         <h1>Lista de tarefas</h1>
 
-        <form onSubmit={this.handleSubmit} action='#' className='form'>
+        <Form
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+          novaTarefa={novaTarefa}
+        />
 
-          <input type="text"
-            onChange={this.handleChange}
-            value={novaTarefa}
-          />
-
-          <button type='submit'>
-            <FaPlus />
-          </button>
-        </form>
-
-        <ul className='tarefas'>
-          {
-            tarefas.map((tarefa, index) => (
-              <li key={tarefa}>
-                {tarefa}
-
-                <span>
-                  <FaEdit
-                    className='edit'
-                    onClick={(e) => this.handleEdit(e, index)}
-                  />
-                  <FaWindowClose
-                    className='delete'
-                    onClick={(e) => this.handleDelete(e, index)}
-                  />
-                </span>
-              </li>
-            ))
-          }
-        </ul>
+        <Tarefas
+          tarefas={tarefas}
+          handleEdit={this.handleEdit}
+          handleDelete={this.handleDelete}
+        />
 
       </div>
     );
---
+++ src/components/Tarefas/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+
+import './Tarefas.css';
+
+export default function Tarefas({ handleEdit, handleDelete, tarefas }) {
+  return (
+    <ul className='tarefas'>
+      {
+        tarefas.map((tarefa, index) => (
+          <li key={tarefa}>
+            {tarefa}
+
+            <span>
+              <FaEdit
+                className='edit'
+                onClick={(e) => handleEdit(e, index)}
+              />
+              <FaWindowClose
+                className='delete'
+                onClick={(e) => handleDelete(e, index)}
+              />
+            </span>
+          </li>
+        ))
+      }
+    </ul>
+  );
+}
+
+Tarefas.propTypes = {
+  handleEdit: PropTypes.func.isRequired,
+  handleDelete: PropTypes.func.isRequired,
+  tarefas: PropTypes.array.isRequired,
+};
